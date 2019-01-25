0101f701bd11b60bbf19c52136a5f1b611aee7e1
thermatk <thermatk@thermatk.com>
2019-01-25 22:55:45 +0100

    remaining GMS(GCM, Wear, etc), except for Maps

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -35,8 +35,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.multidex.MultiDex;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import org.json.JSONObject;
 import org.telegram.messenger.voip.VideoCapturerDevice;
@@ -255,7 +253,7 @@ public class ApplicationLoader extends Application {
         }
 
         ApplicationLoader app = (ApplicationLoader) ApplicationLoader.applicationContext;
-        app.initPushServices();
+        //app.initPushServices();
         if (BuildVars.LOGS_ENABLED) {
             FileLog.d("app initied");
         }
@@ -371,7 +369,7 @@ public class ApplicationLoader extends Application {
             e.printStackTrace();
         }
     }
-
+/*
     private void initPushServices() {
         AndroidUtilities.runOnUIThread(() -> {
             if (getPushProvider().hasServices()) {
@@ -395,6 +393,7 @@ public class ApplicationLoader extends Application {
         }
         return true;
     }
+*/
 
     private static long lastNetworkCheck = -1;
     private static void ensureCurrentNetworkGet() {
--- TMessagesProj/src/main/java/org/telegram/messenger/PushListenerController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/PushListenerController.java
@@ -12,11 +12,6 @@ import androidx.annotation.IntDef;
 import androidx.annotation.Keep;
 import androidx.collection.LongSparseArray;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.messaging.FirebaseMessaging;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.telegram.messenger.voip.VoIPGroupNotification;
@@ -1629,7 +1624,7 @@ public class PushListenerController {
                 if (BuildVars.LOGS_ENABLED) {
                     FileLog.d("FCM Registration not found.");
                 }
-            }
+            }/*
             Utilities.globalQueue.postRunnable(() -> {
                 try {
                     SharedConfig.pushStringGetTimeStart = SystemClock.elapsedRealtime();
@@ -1653,11 +1648,12 @@ public class PushListenerController {
                 } catch (Throwable e) {
                     FileLog.e(e);
                 }
-            });
+            });*/
         }
 
         @Override
         public boolean hasServices() {
+        	return false;/*
             if (hasServices == null) {
                 try {
                     int resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(ApplicationLoader.applicationContext);
@@ -1667,7 +1663,7 @@ public class PushListenerController {
                     hasServices = false;
                 }
             }
-            return hasServices;
+            return hasServices;*/
         }
     }
 }
